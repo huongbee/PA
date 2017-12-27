11d5443c8572875a8082dae2c492e2960b801493
Huong <you@example.com>
2017-12-27 16:55:26 +0700

    8-oop-5

---
+++ 8-oop/5-autoload/index.php
@@ -0,0 +1,8 @@
+<?php
+require_once('init.php');
+
+$a = new A;
+$b = new B;
+$c = new C;
+
+?>
---
+++ 8-oop/5-autoload/init.php
@@ -0,0 +1,8 @@
+<?php
+
+spl_autoload_register(function($className){
+    //echo $className;
+    require_once("$className.php");
+})
+
+?>
