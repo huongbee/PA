771dd2e4a74533eb58d1df999660da1272bf4310
Huong <you@example.com>
2018-01-01 15:58:12 +0700

    1-singleton-2

--- 9-design-pattern/1-singleton.php
+++ 9-design-pattern/1-singleton.php
@@ -1,48 +1,84 @@
 <?php
 
+
+// class UserFactory
+// {
+//     public static function Instance()
+//     {
+//         static $inst = null;
+//         if ($inst === null) {
+//             $inst = new UserFactory();
+//         }
+//         return $inst;
+//     }
+
+// }
+
+// $a = UserFactory::Instance();
+// $b = UserFactory::Instance();
+// var_dump($a);
+// var_dump($b);
+
+// $c = new UserFactory();
+// var_dump($c);
+
+// if($a===$b) echo "bằng nhau";
+// else echo "Khác nhau";
+
+// if($a===$c) echo "bằng nhau";
+// else echo "Khác nhau";
+
+
+// class A{
+//     public static $a;
+//     function __construct(){
+//         if(!self::$a){
+//             self::$a = $this;
+//             echo "Recent create";
+//         }
+//         else{
+//             echo "Created"   ; 
+//         }
+//         return self::$a;
+//     } 
+// }
+
+// $a = new A; //Recent create
+// $a = new A; //Created
+// $c = new A; //Created
+
+
+
 class A{
-    public static $a;
-    function __construct(){
-        if(!self::$a){
-            self::$a = $this;
-            echo "Recent create";
+
+
+    private static $class;
+
+    private function __construct(){
+        echo "class ".__METHOD__.' vua khoi tao'; //get_class()
+    }
+    static function getName(){
+        if(!self::$class){
+            echo "Vua setup";
+            self::$class = new A;
+
         }
         else{
-            echo "Created"   ; 
+            echo "da setup";
         }
-        return self::$a;
-    } 
-}
-
-$a = new A; //Recent create
-$a = new A; //Created
-$c = new A; //Created
-
-
-class UserFactory
-{
-    public static function Instance()
-    {
-        static $inst = null;
-        if ($inst === null) {
-            $inst = new UserFactory();
-        }
-        return $inst;
+        return self::$class;
     }
-
 }
 
-$a = UserFactory::Instance();
-$b = UserFactory::Instance();
-var_dump($a);
-var_dump($b);
+$a = A::getName();
+$a->name = "Huong";
+echo $a->name;
 
-$c = new UserFactory();
-var_dump($c);
+unset($a);
 
-if($a===$b) echo "bằng nhau";
-else echo "Khác nhau";
+//var_dump($a);
 
-if($a===$c) echo "bằng nhau";
-else echo "Khác nhau";
+$b = A::getName();
+echo $b->name;
+
 ?>
