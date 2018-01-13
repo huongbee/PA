e1832a31569f8b71567327c4619d17f4bb6072e4
Huong <you@example.com>
2018-01-13 14:55:37 +0700

    trigger

--- 11-fulltext-search/1.php
+++ 11-fulltext-search/1.php
@@ -3,13 +3,35 @@
 /*
 
 B1:
-	ALTER TABLE `loai_mon_an` ADD FULLTEXT(`ten_loai`,`mo_ta`);
+	ALTER TABLE `foods` ADD FULLTEXT(`name`,`detail`);
 
 B2:
-	SELECT * FROM loai_mon_an WHERE MATCH (ten_loai,mo_ta) AGAINST ("xôi" IN BOOLEAN MODE) 
+	SELECT * FROM `foods` WHERE MATCH(name,detail) AGAINST ("hạt sen" IN BOOLEAN MODE)
 
 Notes:
 - MATCH (ten_loai,mo_ta) tương ứng với  FULLTEXT(`ten_loai`,`mo_ta`)
 	vd: FULLTEXT(`ten_loai`,`mo_ta`) nhưng MATCH (ten_loai) không được
+- NATURAL LANGUAGE MODE  (default)
+
+vd: tìm bài viết có chứa hạt sen:
+	AGAINST ("hạt sen")
+
+	
+- BOOLEAN MODE : tìm những data match với expression: AGAINST ("expression" IN BOOLEAN MODE) 
+ 
+
+1. tìm bài viết match với chữ HẠT  hoặc chữ SEN:
+	AGAINST ("hạt sen" IN BOOLEAN MODE)
+
+2. tìm bài viết match với chữ HẠT , không chứa chữ SEN:
+	AGAINST ("hạt -sen" IN BOOLEAN MODE)
+
+3. tìm bài viết match với chữ HẠT , có hoặc không chứa chữ SEN:
+	AGAINST ("hạt+sen" IN BOOLEAN MODE)
+
+4. tìm bài viết match CHÍNH XÁC với  hạt sen:
+	AGAINST ('"hạt sen"' IN BOOLEAN MODE)
+ 
+
 */
 ?>
